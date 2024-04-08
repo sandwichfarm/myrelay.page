09e874c355eff1a2d65ac23ee36814439f6439c6
dskvr <dskvr@users.noreply.github.com>
2024-04-08 23:51:50 +0200

    fix build error

--- src/lib/components/speedtest.svelte
+++ src/lib/components/speedtest.svelte
@@ -1,30 +1,30 @@
 <script lang="ts">
-  import { Nocap } from '@nostrwatch/nocap'
-  import { onMount, getContext } from 'svelte';
+  // import { Nocap } from '@nostrwatch/nocap'
+  // import { onMount, getContext } from 'svelte';
 
-  import { MY_RELAY_PAGE } from '$lib/contextKeys';
+  // import { MY_RELAY_PAGE } from '$lib/contextKeys';
 
-  let results: any = {}
+  // let results: any = {}
 
-  const Mrp = getContext(MY_RELAY_PAGE);
+  // const Mrp = getContext(MY_RELAY_PAGE);
 
-  Mrp.subscribe((mrp) => {
+  // Mrp.subscribe((mrp) => {
     // const url = mrp.url 
     // const nocap = new Nocap(url)
     // results = nocap.check(['open', 'read', 'write'])
-  });
+  // });
 
 
 </script>
 
-<div class="speedtest">
+<!-- <div class="speedtest">
   speedtest
   {#if Object.keys(results).length}
     <div class="open">{ results.open.duration }</div>
     <div class="read">{ results.read.duration }</div>
     <div class="write">{ results.write.duration }</div>
   {/if}
-</div>
+</div> -->
 <style>
 
 </style>
